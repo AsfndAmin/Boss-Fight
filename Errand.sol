926ee966602de41ab37fa7d4ee5acf4a40c8ff02
AsfndAmin <99183246+AsfndAmin@users.noreply.github.com>
2022-09-10 12:24:37 +0500

    Create Errand.sol

---
+++ Errand.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+interface IBossCardERC1155{
+    function safeTransferFrom(address from, address to, uint id, uint amount, bytes memory data) external;
+}
+
+interface IIngredientsERC1155{
+    function safeTransferFrom(address from, address to, uint id, uint amount, bytes memory data) external;
+    function mint(address to, uint256 id, uint256 value) external returns(address);
+}
+
+contract Errand is Initializable,ERC721HolderUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable,UUPSUpgradeable{
+
+    
+
+
+}
